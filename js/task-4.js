e3f3fc876241043e820dd2e45f4cb5d17d297762
OlexandrVakulin <vlipoq@gmail.com>
2025-06-01 16:20:02 +0200

    add_files-hwt4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
+let base = {};
+
+const fooForm = loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (email === "" || password === "") {
+        return alert('All form fields must be filled in');
+    }
+
+    base.email = email.trim();
+    base.password = password.trim();
+
+    console.log(base);
+  form.reset();
+
+});
+
 
