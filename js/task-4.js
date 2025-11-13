d81ccbeeb65f074a20a0bf1968f3a2aa769906c0
Alex <vlipoq@gmail.com>
2025-11-13 11:49:54 +0100

    Implement login form validation and styles
    Refactor login form submission and styling.

--- js/task-4.js
+++ js/task-4.js
@@ -1,39 +1,74 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+const loginForm = document.querySelector(".login-form");
+let base = {};
 
-const button = document.querySelector(".change-color");
-const spanColor = document.querySelector(".color");
-const backgroundColor = document.querySelector("body");
+const fooForm = loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const email = form.elements.email.value;
+    const password = form.elements.password.value;
 
-button.addEventListener("click", () => {
-  backgroundColor.style.backgroundColor = getRandomHexColor();
-  spanColor.textContent = getRandomHexColor();
+    if (email === "" || password === "") {
+        return alert('All form fields must be filled in');
+    }
+
+    base.email = email.trim();
+    base.password = password.trim();
+    
+  form.reset();
+
 });
+
+
 
 // table style
 
-const widget = document.querySelector(".widget");
+// const email = document.querySelector("input email");
+// email.placeholder = "vfdv";
 
-widget.style.width = "345px";
-widget.style.height = "285px";
-widget.style.padding = "100px 0";
-widget.style.margin = "0 auto";
+  loginForm.style.fontWeight = "400";
+  loginForm.style.fontFamily = "'Montserrat', sans-serif";
+  loginForm.style.fontSize = "16px";
+  loginForm.style.letterSpacing = "0.04m";
+  loginForm.style.color = "#2e2f42";
+  loginForm.style.maxWidth = "408px";
+  loginForm.style.height = "256px";
+  loginForm.style.padding = "24px";
+  loginForm.style.margin = "0 auto";
+
+const textInputs = document.querySelectorAll("input");
 
 
+textInputs.forEach((textInput) => {
 
-const p = document.querySelector(".widget p");
+  textInput.style.fontWeight = "400";
+  textInput.style.fontFamily = "'Montserrat', sans-serif";
+  textInput.style.fontSize = "16px";
+  textInput.style.letterSpacing = "0.04m";
+  textInput.style.color = "#2e2f42";
+  textInput.style.border = "1px solid #808080";
+  textInput.style.borderRadius = "4px";
+  textInput.style.width = "360px";
+  textInput.style.height = "40px";
+  textInput.style.padding = "8px 16px";
+  textInput.style.margin = "8px 0";
 
-p.style.fontWeight = "400";
-p.style.fontFamily = "'Montserrat', sans-serif";
-p.style.fontSize = "16px";
-p.style.letterSpacing = "0.04m";
-p.style.color = "#2e2f42";
-p.style.textAlign = "center";
-p.style.margin = "0 0 16px 0";
+  textInput.addEventListener('mouseenter', () => {
+    textInput.style.border = "1px solid #000000";
+  });
+  
+  textInput.addEventListener('mouseleave', () => {
+    textInput.style.border = "1px solid #808080";
+  });
+  
+  textInput.addEventListener('focus', () => {
+    textInput.placeholder = "Type area";
+  })
 
+
+})
+
+
+const button = document.querySelector("button");
 
 button.style.fontWeight = "500";
 button.style.fontFamily = "'Montserrat', sans-serif";
@@ -44,7 +79,7 @@ button.style.background = "#4e75ff";
 button.style.border = "none";
 button.style.borderRadius = "8px";
 button.style.padding = "8px 16px";
-button.style.margin = "8px 100px";
+button.style.margin = "8px 140px";
 
 button.addEventListener('mouseenter', () => {
   button.style.background = "#6c8cff";
@@ -53,3 +88,5 @@ button.addEventListener('mouseenter', () => {
 button.addEventListener("mouseleave", () => {
   button.style.background = "#4e75ff";
 });
+
+
