a9370ce9c7aae2912b205f4789a6c8a8daaf3d8d
Alex <vlipoq@gmail.com>
2025-11-13 09:49:48 +0100

    add errors for AI test

--- js/task-4.js
+++ js/task-4.js
@@ -1,33 +1,55 @@
-const base = {
-  users: [],
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
-  getUsers() {
-    return this.users;
-  },
-  
-  addUser(newUser) {
-    this.users.push(newUser);
-  }
-  
-};
+const button = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+const backgroundColor = document.querySelector("body");
 
-const loginForm = document.querySelector(".login-form");
-
-const fooForm = loginForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const inEmail = form.elements.email.value.trim();
-    const inPassword = form.elements.password.value.trim();
-  
-    if (inEmail === "" || inPassword === "") {
-        return alert('All form fields must be filled in');
-    }
-  base.addUser({ email: inEmail, password: inPassword });
-  
-  console.table(base.getUsers());
-      
-  form.reset();
-
+button.addEventListener("click", () => {
+  backgroundColor.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
 });
 
+// table style
 
+const widget = document.querySelector(".widget");
+
+widget.style.width = "345px";
+widget.style.height = "285px";
+widget.style.padding = "100px 0";
+widget.style.margin = "0 auto";
+
+
+
+const p = document.querySelector(".widget p");
+
+p.style.fontWeight = "400";
+p.style.fontFamily = "'Montserrat', sans-serif";
+p.style.fontSize = "16px";
+p.style.letterSpacing = "0.04m";
+p.style.color = "#2e2f42";
+p.style.textAlign = "center";
+p.style.margin = "0 0 16px 0";
+
+
+button.style.fontWeight = "500";
+button.style.fontFamily = "'Montserrat', sans-serif";
+button.style.fontSize = "16px";
+button.style.letterSpacing = "0.04m";
+button.style.color = "#ffffff";
+button.style.background = "#4e75ff";
+button.style.border = "none";
+button.style.borderRadius = "8px";
+button.style.padding = "8px 16px";
+button.style.margin = "8px 100px";
+
+button.addEventListener('mouseenter', () => {
+  button.style.background = "#6c8cff";
+});
+
+button.addEventListener("mouseleave", () => {
+  button.style.background = "#4e75ff";
+});
